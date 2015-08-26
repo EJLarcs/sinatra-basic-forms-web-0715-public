4a3e7fe989351c7806d28bcf3f750eb590324ae4
EJLarcs <ejlarcombe@gmail.com>
2015-08-26 15:13:54 -0400

    done

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -10,6 +10,26 @@ class App < Sinatra::Base
     @song = Song.find(params[:id])
     erb :'songs/show'
   end
-  
-end
 
+  get '/songs/:id/edit' do
+    @song = Song.find(params[:id])
+    erb :'songs/edit'
+  end
+
+  post '/songs/:id' do
+    @song = Song.find(params[:id])
+    erb :'songs/show'
+    # song = Song.find(params[:id])
+    # song.update(params[:song])
+    # redirect "/songs/#{@song.id}"
+  end
+
+  patch '/songs/:id' do
+    song = Song.find(params[:id])
+    song.update(params[:song])
+    redirect "/songs/#{@song.id}"
+    #redirect "...." so we cansee that something has been updated.. maybe lets look into this
+    #packaged in such a way that this will give the key value pair
+  end
+
+end
